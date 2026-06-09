9114bdc231a2907a7f3731d40f081a6fd3b27472
gunelsuleymanova <gunels4242@gmail.com>
2026-06-09 14:05:51 +0400

    asd

---
+++ src/test/java/com/example/Art/ArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
